3047bad9ff9fbda941a8af0b7ffae73b61b21a2e
tany2-apt <tanya.bharti@aptusdatalabs.com>
2021-04-23 10:33:12 +0530

    go file changed for checking--watchtower

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 func home(w http.ResponseWriter, r *http.Request) {
 
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(`{"message": "hello world"}`))
+	w.Write([]byte(`{"message": "CHECK WATCHTOWER"}`))
 
 }
 
